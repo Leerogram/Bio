21165bd51405e9ab1550a4947622411e2d0cca84
Leesangil <ehxhflakqk@gmail.com>
2020-01-20 18:08:56 +0900

    2020 01 20 게시판 등록, 수정, 삭제

--- src/main/java/com/Bio/silee/domain/posts/service/PostsService.java
+++ src/main/java/com/Bio/silee/domain/posts/service/PostsService.java
@@ -2,13 +2,16 @@ package com.Bio.silee.domain.posts.service;
 
 import com.Bio.silee.domain.posts.Posts;
 import com.Bio.silee.domain.posts.PostsRepository;
+import com.Bio.silee.web.dto.PostsListResponseDto;
 import com.Bio.silee.web.dto.PostsUpdateRequestDto;
 import com.Bio.silee.web.dto.PostsResponseDto;
 import com.Bio.silee.web.dto.PostsSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,5 +36,20 @@ public class PostsService {
         Posts entity = postsRepository.findById(id)
                 .orElseThrow(() ->new IllegalArgumentException("해당사용자가 없습니다. id=" + id));
         return  new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(
+                "해당 게시글이 없습니다. id =" + id
+        ));
+        postsRepository.delete(posts);
     }
 }
--- src/test/java/com/Bio/silee/web/IndexControllerTest.java
+++ src/test/java/com/Bio/silee/web/IndexControllerTest.java
@@ -1,11 +1,15 @@
 package com.Bio.silee.web;
 
+import com.Bio.silee.web.dto.PostsResponseDto;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
